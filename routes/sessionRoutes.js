70e6338c95577ff3400f2fb66a685564ff92efac
vAIF LLC <vaifappios@gmail.com>
2023-09-02 22:46:55 -0400

    fixed session bug

--- routes/sessionRoutes.js
+++ routes/sessionRoutes.js
@@ -1,9 +1,8 @@
 const express = require("express")
 const SessionController = require("../controllers/SessionController")
-const expressJwt = require("express-jwt")
+const { expressjwt: expressJwt } = require("express-jwt")
 require("dotenv").config()
 const User = require("../models/User")
-
 const authenticateJWT = expressJwt({
   secret: process.env.JWT_SECRET,
   algorithms: ["HS256"],
@@ -20,60 +19,50 @@ const authenticateJWT = expressJwt({
   },
 }).unless({ path: ["/users/register", "/users/login"] })
 
-const requireLogin = async (req, res, next) => {
+const requireLogin = (req, res, next) => {
   try {
-    if (req.auth && req.auth.userId) {
-      const user = await User.findOne({ userId: req.auth.userId })
-      if (user) {
-        return next()
+    authenticateJWT(req, res, async (err) => {
+      if (err) {
+        return res.status(401).json({ message: err.message })
       }
-    }
+
+      // Extract the userId from req.auth
+      const userId = req.auth ? req.auth.userId : null
+
+      if (!userId) {
+        return res
+          .status(401)
+          .json({ message: "Invalid token or no token provided." })
+      }
+
+      // Find the user in the database using the custom userId
+      const user = await User.findOne({ userId: userId })
+
+      if (!user) {
+        return res.status(401).json({ message: "The user does not exist." })
+      }
+
+      next()
+    })
     res.status(401).json({ message: "Unauthorized" })
   } catch (error) {
     res.status(500).json({ message: "Internal Server Error" })
   }
 }
 
+
 const router = express.Router()
 
+// Routes for session management
 // Consolidated Routes for session management
-router.post(
-  "/sessions/create",
-  authenticateJWT,
-  requireLogin,
-  SessionController.createSession
-)
-router.get(
-  "/sessions/:userId",
-  authenticateJWT,
-  requireLogin,
-  SessionController.getSessions
-)
-router.post(
-  "/sessions/revoke",
-  authenticateJWT,
-  requireLogin,
-  SessionController.revokeSession
-)
+router.post("/sessions/create", authenticateJWT, requireLogin, SessionController.createSession);
+router.get("/sessions/:userId", authenticateJWT, requireLogin, SessionController.getSessions);
+router.post("/sessions/revoke", authenticateJWT, requireLogin, SessionController.revokeSession);
 
 // Routes for toggling logs
-router.post(
-  "/activity-logs/toggle",
-  authenticateJWT,
-  requireLogin,
-  SessionController.toggleActivityLogs
-)
-router.post(
-  "/auth-logs/toggle",
-  authenticateJWT,
-  requireLogin,
-  SessionController.toggleAuthLogs
-)
-router.post(
-  "/advanced-logs/toggle",
-  authenticateJWT,
-  requireLogin,
-  SessionController.toggleAdvancedLogs
-)
+router.post("/activity-logs/toggle", authenticateJWT, requireLogin, SessionController.toggleActivityLogs);
+router.post("/auth-logs/toggle", authenticateJWT, requireLogin, SessionController.toggleAuthLogs);
+router.post("/advanced-logs/toggle", authenticateJWT, requireLogin, SessionController.toggleAdvancedLogs);
+
 
 module.exports = router
